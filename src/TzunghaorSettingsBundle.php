f0a9d9a19d08779db6c712500b4aba94ee8a32e2
Tac Tacelosky <tacman@gmail.com>
2022-08-24 09:07:38 -0400

    fix bundle path

--- src/TzunghaorSettingsBundle.php
+++ src/TzunghaorSettingsBundle.php
@@ -4,8 +4,23 @@
 namespace Tzunghaor\SettingsBundle;
 
 
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TzunghaorSettingsBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function getPath(): string
+    {
+        if (null === $this->path) {
+            $reflected = new \ReflectionObject($this);
+            // assume the modern directory structure by default
+            $this->path = \dirname($reflected->getFileName(), 2);
+        }
+
+        return $this->path;
+    }
+
 }
