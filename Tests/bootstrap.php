7b32eaa1057aaf9596d47c350fe22d86829200b1
Andras Prim <andras.prim@textbroker.eu>
2026-06-19 16:03:02 +0200

    - debug phpunit

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,12 @@
 <?php
+
+use SebastianBergmann\Comparator\Factory;
+use Tzunghaor\SettingsBundle\Test\Helper\TzunghaorObjectComparator;
+
 require_once (__DIR__ . '/../vendor/autoload.php');
 
-die("bootstrap OK\n");
+Factory::getInstance()->register(new TzunghaorObjectComparator());
+
 // clear test project's cache to force a container rebuild
 $fileSystem = new \Symfony\Component\Filesystem\Filesystem();
 $fileSystem->remove(__DIR__ . '/TestApp/var/cache/minimal');
